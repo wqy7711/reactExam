ef89c1f21ce6f35341ef5cc516ddc5f69500a1ed
unknown <944325009@qq.com>
2024-11-08 17:06:23 +0000

    Added Original Language attribute

--- src/components/movieDetails/index.js
+++ src/components/movieDetails/index.js
@@ -9,6 +9,7 @@ import Fab from "@mui/material/Fab";
 import Typography from "@mui/material/Typography";
 import Drawer from "@mui/material/Drawer";
 import MovieReviews from "../movieReviews"
+import LanguageIcon from "@mui/icons-material/Language";
 
 
 const root = {
@@ -59,6 +60,10 @@ const MovieDetails = ({ movie }) => {  // Don't miss this!
           label={`${movie.vote_average} (${movie.vote_count}`}
         />
         <Chip label={`Released: ${movie.release_date}`} />
+        <Chip 
+          icon={<LanguageIcon />} 
+          label={`Original Language: ${movie.original_language}`} 
+        />
       </Paper>
       
       <Fab
